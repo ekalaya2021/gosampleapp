acd8cd1056f00766cda773850a74fc8dc7366314
Ridwan Fardani <ridwan.fardani@infokes.co.id>
2024-01-05 19:16:30 +0700

    disable spot instance

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         res := strings.TrimPrefix(html.EscapeString(r.URL.Path),"/")
-        fmt.Fprintf(w, "Bonjour %q ", res)
+        fmt.Fprintf(w, "Wilujeng sumping %q ", res)
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
