43fa877ccd128c5264cf09b4f161ad1e6b781094
Ridwan Fardani <ridwan.fardani@infokes.co.id>
2024-01-08 14:13:12 +0700

    change code

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         res := strings.TrimPrefix(html.EscapeString(r.URL.Path),"/")
         fmt.Fprintf(w, "Bonjour %q ", res)
+        fmt.Printf("%q\n", []byte("안녕"))
     })
 
     log.Fatal(http.ListenAndServe(":8081", nil))
